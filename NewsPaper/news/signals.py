142449867e9bfd1fc4b7bcf2d04881db9993cd7b
smolnikov-ai <smolnikov-ai@mail.ru>
2025-04-04 07:33:12 +0300

    HW-03 added addressing the subscriber in emails

--- NewsPaper/news/signals.py
+++ NewsPaper/news/signals.py
@@ -9,33 +9,70 @@ from news.models import PostCategory, Category
 
 
 def send_notifications(preview, pk, title, subscribers):
-    html_content = render_to_string(
-        'post_created_email.html',
-        {
-            'text': preview,
-            'link': f'{settings.SITE_URL}/news/{pk}',
-        }
-    )
+    for s in subscribers:
+        html_content = render_to_string(
+            'post_created_email.html',
+            {
+                'text': preview,
+                'username': s.username,
+                'link': f'{settings.SITE_URL}/news/{pk}',
+            }
+        )
 
-    msg = EmailMultiAlternatives(
-        subject=title,
-        body='',
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        to=subscribers,
-    )
+        msg = EmailMultiAlternatives(
+            subject=title,
+            body='',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to=[s.email, ],
+        )
 
-    msg.attach_alternative(html_content, 'text/html')
-    msg.send()
+        msg.attach_alternative(html_content, 'text/html')
+        msg.send()
 
 
 @receiver(m2m_changed, sender=PostCategory)
 def notify_about_new_post(sender, instance, **kwargs):
     if kwargs['action'] == 'post_add':
         categories = instance.categories.all()
-        subscribers_emails = []
+        subscribers = []
 
         for cat in categories:
-            subscribers = cat.subscribers.all()
-            subscribers_emails += [s.email for s in subscribers]
+            subscribers += cat.subscribers.all()
 
-        send_notifications(instance.preview(), instance.pk, instance.title, subscribers_emails)
+        send_notifications(instance.preview(), instance.pk, instance.title, set(subscribers))
+
+
+
+# # отправка сообщения на почту username при добавлении новых статей на портал
+# # отправка происходит сразу списку пользователей
+# def send_notifications(preview, pk, title, subscribers):
+#     html_content = render_to_string(
+#         'post_created_email.html',
+#         {
+#             'text': preview,
+#             'link': f'{settings.SITE_URL}/news/{pk}',
+#         }
+#     )
+#
+#     msg = EmailMultiAlternatives(
+#         subject=title,
+#         body='',
+#         from_email=settings.DEFAULT_FROM_EMAIL,
+#         to=subscribers,
+#     )
+#
+#     msg.attach_alternative(html_content, 'text/html')
+#     msg.send()
+#
+#
+# @receiver(m2m_changed, sender=PostCategory)
+# def notify_about_new_post(sender, instance, **kwargs):
+#     if kwargs['action'] == 'post_add':
+#         categories = instance.categories.all()
+#         subscribers_emails = []
+#
+#         for cat in categories:
+#             subscribers = cat.subscribers.all()
+#             subscribers_emails += [s.email for s in subscribers]
+#
+#         send_notifications(instance.preview(), instance.pk, instance.title, subscribers_emails)
