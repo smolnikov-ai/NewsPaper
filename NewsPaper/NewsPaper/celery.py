b2e4a14a920757e0529273dc9b4e4b98a415fcfa
smolnikov-ai <smolnikov-ai@mail.ru>
2025-04-10 14:49:37 +0300

    HW-03 install & settings Celery & Redis

---
+++ NewsPaper/NewsPaper/celery.py
@@ -0,0 +1,15 @@
+import os
+from celery import Celery
+
+
+# Связываем настройки Django с настройками Celery через переменную окружения
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsPaper.settings')
+
+# создаем экземпляр приложения Celery и устанавливаем для него файл конфигурации
+app = Celery('NewsPaper')
+# указываем пространство имен, чтобы Celery сам находил необходимые настройки в общем конфигурационном
+# файле settings.py. Он их будет искать по шаблону "CELERY_***"
+app.config_from_object('django.conf:settings', namespace = 'CELERY')
+
+# указываем Celery автоматически искать задания в файлах tasks.py каждого приложения проекта
+app.autodiscover_tasks()
--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -183,3 +183,15 @@ APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
 
 # если задача не выполняется за 25 секунд, то она автоматически снимается, можете поставить время побольше, но как правило, это сильно бьёт по производительности сервера
 APSCHEDULER_RUN_NOW_TIMEOUT = 25  # Seconds
+
+# настройки для Celery и Redis
+# указывает на URL брокера сообщений (Redis). По умолчанию он находится на порту 6379
+CELERY_BROKER_URL = 'redis://localhost:6379'
+# указываеv на хранилище результатов выполнения задач
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+#допустимый формат данных
+CELERY_ACCEPT_CONTENT = ['application/json']
+#метод сериализации задач
+CELERY_TASK_SERIALIZER = 'json'
+#метод сериализации результатов
+CELERY_RESULT_SERIALIZER = 'json'
