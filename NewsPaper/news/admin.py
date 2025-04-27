0813c435d899398e21359db38c110baa864e7e4f
smolnikov-ai <smolnikov-ai@mail.ru>
2025-04-27 13:57:29 +0300

    HW-03 upd admin panel

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Post, Category
 
-admin.site.register(Post)
+
+# создаем новый класс для представления товаров в admin panel
+class PostAdmin(admin.ModelAdmin):
+    # list_display - это список или кортеж с полями, которые будут отображаться в таблице в admin panel
+    list_display = ['title','date_time_in', 'author',]
+    # list_filter - примитивные фильтра в админке
+    list_filter = ['categories', 'author', 'type',]
+    # search_fields - поиск по указанным полям
+    search_fields =  ['title', 'content']
+
+
+# регистрация моделей
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
